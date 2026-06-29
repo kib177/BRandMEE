109d3f09b5e84230474d1c7971dcea096b6fba3c
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-06-29 16:25:42 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
     "default-src 'self' blob:; " +
-    "script-src 'self' https://unpkg.com blob:; " +
+    "script-src 'self' blob:; " +
     "worker-src 'self' blob:; " +
     "style-src 'self' 'unsafe-inline'; " +
     "img-src 'self' data:; " +
