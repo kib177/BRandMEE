b2e763c5f353f699290d8b0bcf59543395e9071d
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-06-29 13:55:39 +0300

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,7 +1,7 @@
  const API_BASE = '/api/inventory';
 
 async function apiFetch(url, options = {}) {
-    const res = await apiFetch(url, {
+    const res = await Fetch(url, {
         ...options,
         headers: {
             ...options.headers,
