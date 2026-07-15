185d8dc887a7fbfd71674af8ad060c73faef0bae
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-07-15 10:43:13 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@
  * Licensed under MIT
  */
 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const inventoryRoutes = require('./routes/inventory');
 const cron = require('node-cron');
 const { sendMail } = require('./mailer');
 const db = require('./db');
@@ -42,7 +42,7 @@ app.use(cors({
 app.use(express.json({ limit: '5mb' }));
 
 // API маршруты
-app.use('/api/inventory', inventoryRoutes);
+app.use('/api/inventory', require('./routes/inventory'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/write-offs', require('./routes/writeoffs'));
 app.use('/api/directories', require('./routes/directories'));
