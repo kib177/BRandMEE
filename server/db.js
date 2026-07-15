f1ca63abaa71ee3b71d67a69f6d7c512d2e03aab
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-07-15 10:29:48 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -7,11 +7,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'warehouse_db',
-  user: process.env.DB_USER || 'warehouse_admin',
-  password: process.env.DB_PASSWORD || 'твой_пароль',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   max: 20,
   idleTimeoutMillis: 30000
 });
