99fd327ff85a117aa02ae156f62372dc0a1c3e04
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-07-15 13:29:36 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/write-offs', require('./routes/writeoffs'));
 app.use('/api/directories', require('./routes/directories'));
 app.use('/api/users', require('./routes/users'));
-app.use('/api/mailing_admin', require('./routes/mailing_admin'));
+app.use('/api/mailing', require('./routes/mailing'));
 
 // Статические файлы (фронтенд)
 app.use(express.static(path.join(__dirname, '..', 'public')));
