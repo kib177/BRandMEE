4500d86070a2279e0b86435dac976256db732c23
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-07-16 15:27:12 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -42,6 +42,7 @@ app.use(cors({
 app.use(express.json({ limit: '5mb' }));
 
 // API маршруты
+app.use('/api/audit-log', require('./routes/audit-log'));
 app.use('/api/inventory', require('./routes/inventory'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/write-offs', require('./routes/writeoffs'));
