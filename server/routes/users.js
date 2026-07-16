0208766aa3ac37158b7092fb52171beb6d2e099c
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-07-16 15:57:09 +0300

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const pool = require('../db');
 const { authMiddleware, requireRole } = require('../middleware/auth');
-const { logAction } = require('../log/logger');
 
 // ======== ПОЛЬЗОВАТЕЛИ ========
 
