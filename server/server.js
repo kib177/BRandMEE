a6ea43148b729feed6c2c7a22ac31817af682afb
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-08-11 14:07:36 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ app.use(compression());
 
 // CSP middleware
 app.use((req, res, next) => {
+   res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
   res.setHeader(
     'Content-Security-Policy',
     "default-src 'self' blob:; " +
